97f686edeaec2612b7e0a0fc41d26f0b900e803c
suxg32732 <853032732@qq.com>
2016-12-15 21:03:47 +0800

    1215.2

--- 把数组组成最小的书/ToMinNunber/src/Solution.java
+++ 把数组组成最小的书/ToMinNunber/src/Solution.java
@@ -2,6 +2,8 @@
  * Created by Administator on 2016/12/15.
  */
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public  class Solution {
     public static String PrintMinNumber(int [] numbers) {
@@ -11,7 +13,23 @@ public  class Solution {
         for (int i : numbers) {
             ls.add(String.valueOf(i));
         }
-        ls.sort(new java.util.Comparator<String>() {
+//        ls.sort(new java.util.Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                for (int i = 0; i < o1.length() && i < o2.length(); i++) {
+//                    if (o1.charAt(i) > o2.charAt(i))
+//                        return 1;
+//                    if (o1.charAt(i) < o2.charAt(i))
+//                        return -1;
+//                }
+//                if (o1.length() == o2.length())
+//                    return 0;
+//                else if (o1.length() > o2.length())
+//                    return -1;
+//                return 1;
+//            }
+//        });
+        Collections.sort(ls, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 for (int i = 0; i < o1.length() && i < o2.length(); i++) {
