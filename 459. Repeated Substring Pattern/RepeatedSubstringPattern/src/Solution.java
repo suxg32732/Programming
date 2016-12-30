21369c525d67175f36a15c57deb68ed7db7be1fc
suxg32732 <853032732@qq.com>
2016-12-30 19:53:18 +0800

    1230 没有写完

---
+++ 459. Repeated Substring Pattern/RepeatedSubstringPattern/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administator on 2016/12/30.
+ */
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+
+    }
+    public String getSinglePattern(String str) {
+        StringBuffer sbf = new StringBuffer();
+        for (int i );
+        return sbf.toString();
+    }
+}
