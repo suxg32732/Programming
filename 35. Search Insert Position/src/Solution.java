dd75b29923f575a86c2bc2133ffdac4cb0e360ed
suxg32732 <853032732@qq.com>
2017-07-27 22:04:39 +0800

    折半查找结束条件一定是 low > high

--- 35. Search Insert Position/src/Solution.java
+++ 35. Search Insert Position/src/Solution.java
@@ -5,6 +5,20 @@ public class Solution {
                 return i;
             }
         }
-        return nums.length;
+       return nums.length;
+    }
+    int method(int[] nums, int target) {
+        if (nums[0] >= target) return 0;
+        if (nums[nums.length - 1] < target) return nums.length;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
     }
 }
