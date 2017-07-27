bebc3d2bd4831ca678093fd765eabebcc8be31ae
suxg32732 <853032732@qq.com>
2017-07-27 13:33:26 +0800

    2017.7.27.13

---
+++ 557. Reverse Words in a String III/src/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public static  String reverseWords(String s) {
+        StringBuilder stb = new StringBuilder();
+        for (int i = 0; i < s.length();) {
+            int j = i;
+            while (j < s.length() && s.charAt(j) != ' ')
+                j++;
+            boolean flag = false;
+            if (j < s.length()) flag = true;
+            for(; j - 1 >= i; j--) {
+                stb.append(s.charAt(j-1));
+            }
+            if (flag) stb.append(' ');
+            i = stb.length();
+        }
+        return stb.toString();
+    }
+
+    public String method2(String s) {
+        StringBuilder stb = new StringBuilder();
+        for (String ss : s.split(" ")) {
+            stb.append(' ').append(new StringBuilder(ss).reverse());
+        }
+        return stb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String string = "Let's make";
+        System.out.println(" he ".trim());
+//     System.out.println(reverseWords(string));
+    }
+}
