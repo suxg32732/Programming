f194fa06fe96b13cb79ef5d874547fa6f41fe864
suxg32732 <853032732@qq.com>
2017-07-27 18:10:40 +0800

    18.10

--- 485. Max Consecutive Ones/src/Solution.java
+++ 485. Max Consecutive Ones/src/Solution.java
@@ -24,5 +24,6 @@ public class Solution {
   public static void main(String[] args) {
     int[] n = {1,1,1,1,0,0,0,1,1,1,};
     System.out.println(findMaxConsecutiveOnes(n));
+    System.out.println(1^1^3);
   }
 }
